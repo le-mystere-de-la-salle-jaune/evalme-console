ff3d9171558be83bd06032ecf4cd69c2848f0fe5
Mayeul Grivet-Roux <gmayeul@yahoo.fr>
2018-07-18 17:41:53 +0200

    fix URL prod

--- module-duel/service.js
+++ module-duel/service.js
@@ -3,7 +3,7 @@ exports.lister = function (callback) {
     var request = require('request')
 
     // Envoie de la requête http
-    request('http://localhost:8080/api/duels', { json: true }, function (err, res, body) {
+    request('https://evalme-back.herokuapp.com/api/duels', { json: true }, function (err, res, body) {
         if (err) { return console.log('Erreur', err); }
 
         // transmission de la réponse grâce à la technique du callback
