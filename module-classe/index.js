39fc10cf236c4a3453174c60ff07c8665913f621
TREMION Maxime <maxime.tremion@hotmail.fr>
2018-07-20 15:23:14 +0200

    pause

--- module-classe/index.js
+++ module-classe/index.js
@@ -3,40 +3,71 @@ var TITRE = 'Classe'
 var service = require('../module-classe/service')
 var request = require('request')
 // fonction démarrer exprimant la logique de votre entité
-var demarrer = function(rl) {
+var demarrer = function(rl) 
+{
 
     // exemple d'affichage de menu
-    lg("*** Entité "+TITRE+" ***");
-    lg("1. Lister les classes");
-    lg("2. Créer une nouvelle classe");
+    lg(`*** Entité ${TITRE} ***`);
+    lg(`1. Lister les classes`);
+    lg(`2. Créer une nouvelle classe`);
 
 
     // récupération du choix
-    rl.question("Votre choix : ", function(numeroChoix) {
+    rl.question(`Votre choix :`, function(numeroChoix) 
+    {
 
         // une fois la saisie effectuée
 
-        if(numeroChoix == 1) {
-            lg(">>>> Vous avez choisi de lister");
-            lg("ID classe || Nom classe")
-            lg("-----------------------")
-            service.lister(function(uneListe) {
-                uneListe.forEach(element => {
-                    console.log("    "+element.id +"     ||"+"    "+element.nom);
-                });
-            });
-        } else if (numeroChoix == 2) {
-            lg(">>>> Vous avez choisi de créer une nouvelle classe");
-            rl.question("Le nom de votre classe : ", function(nomClasse) {
-                service.creer(sonNom)
-            }
-        )
+        if(numeroChoix == 1) 
+        {
+            lg(`>>>> Vous avez choisi de lister`)
+            lg(`ID classe || Nom classe`)
+            lg(`-----------------------`)
+
+            service.lister(function(uneListe) 
+            {
+                uneListe.forEach(element => 
+                {
+                    console.log(`    ${element.id}     ||    ${element.nom}`)
+                })
+            })
+
+            lg(`*** Souhaitez vous voir les détails d'une ${TITRE} ? ***`)
+            lg(`1. Oui`)
+            lg(`2. Non`)
+
+            rl.question(`Votre choix :`, function(numeroChoix) 
+            {
+                if(numeroChoix == 1) 
+                {
+                    lg(`*** Entrez l'id de la classe que vous souhaitez examiner ***`)
+
+                    rl.question(`Id :`, function(id) 
+                    {
+                        service.detail(function(id,uneClasse) 
+                        {
+                                console.log(uneClasse)
+                        })
+                    })
+                }
+            })
+        
+        } 
+        else if (numeroChoix == 2) 
+        {
+            lg(`>>>> Vous avez choisi de créer une nouvelle classe`);
+
+            rl.question("Le nom de votre classe : ", function(nomClasse) 
+                {
+                    service.creer(sonNom)
+                }
+            )
         }
 
         // permet d'arrêter l'application
         // à n'invoquer que si l'on ne servira plus de la saisie utilisateur
         rl.close();
-    });
+    })
 };
 
 
--- module-classe/service.js
+++ module-classe/service.js
@@ -1,24 +1,40 @@
 lg = console.log
 
 exports.lister = function (callback) {
-    var request = require('request')
+    const request = require('request')
 
 // Envoie de la requête http
-request('http://localhost:8080/api/classes', { json: true }, function(err, res, body) {
+request('http://localhost:8080/api/classes/lister', { json: true }, function(err, res, body) {
     if (err) { return lg('Erreur', err); }
 
     // body contient les données récupérées
     callback(body);
 })};
 
+exports.detail = function (id,callback)
+{
+    const request = require('request')
+
+    request(`http://localhost:8080/api/classes/lister/${id}`, { json: true }, function(err, res, body) 
+    {
+        if (err) 
+        { 
+            return lg('Erreur', err) 
+        }
+
+    // body contient les données récupérées
+    callback(body);
+    })
+}
+
 
 exports.creer = function(sonNom) {
-    var liste = lister
-    var newClasse = 
+    const liste = lister
+    const newClasse = 
     {
         id: liste.length+1,
         nom: sonNom
     }
     liste.add(newClasse)
-    lg("classe créée !")
+    lg(`Classe ${newClasse.name} créée !`)
 }
