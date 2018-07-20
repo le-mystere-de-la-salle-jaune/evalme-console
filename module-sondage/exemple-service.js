ddb40a8d306ec92d7e856716cf690e52a5c25fcb
Kaczmarek Noel <kaczmarek.no@gmail.com>
2018-07-20 10:44:55 +0200

    commit avant es2015+

--- module-sondage/exemple-service.js
+++ module-sondage/exemple-service.js
@@ -1,6 +1,6 @@
 var service = require('../module-sondage/service')
 
 // exemple d'utilisation de la liste.
-service.lister(function(uneListe) {
-    console.log(uneListe);
+service.lister(function(sondages) {
+    console.log(sondages);
 });
